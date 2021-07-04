7b22015f3cc550ba0e23e9fdb85ec74e7778bd38
Abraham Palomino <abraham@propulsar.com>
2021-07-04 13:16:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post("/send", (req, res) => {
     transporter.sendMail(mail, (err, data) => {
       if (err) {
         console.log(err);
-        res.status(500).send("Algo mal tamal.");
+        res.status(500).send(err);
       } else {
         res.status(200).send("Email successfully sent to recipient!");
       }
