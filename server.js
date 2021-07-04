9a79e06ba17b168967e8a302fc225e894e8bc614
Abraham Palomino <abraham@propulsar.com>
2021-07-04 13:51:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   auth: {
-    user: "contacto@propulsar.com",
-    pass:"contacto1234",
+    user: process.env.EMAIL,
+    pass:process.env.PASS,
   },
 });
 
